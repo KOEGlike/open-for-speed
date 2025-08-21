01caac5857e2c74b5062360f5348a83930a6719e
Balázs Marcell <KoegLike@proton.me>
2025-08-21 11:07:10 +0300

    found a great tutorial on motorcycles

--- vehicle_body_3d.gd
+++ vehicle_body_3d.gd
@@ -4,5 +4,11 @@ extends VehicleBody3D
 @export var engine_power=1000
 
 func _physics_process(delta: float) -> void:
+	#axis_lock_angular_x=true
+	#axis_lock_angular_y=true
+	#wwwwwwwwaxis_lock_angular_z=true
 	steering=move_toward(steering, Input.get_axis("right","left") * max_steer, delta*10)
 	engine_force=Input.get_axis("down", "up") * engine_power
+	
+# func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	
